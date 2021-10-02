0835ddc31c4edaa5fbc1d1c6e9f1c4fcef048a60
@kozminskii_fedor <fvk.1972@gmail.com>
2021-10-02 14:08:51 +0300

    Диплом- requests, команды /lowprice, /history

--- botrequests/__init__.py
+++ botrequests/__init__.py
@@ -1,1 +1,3 @@
 __all__ = ['bestdeal', 'help', 'history', 'highprice', 'lowprice', 'start', 'unexpected_case']
+
+from . import dysplay_output
---
+++ buttons/photos_keyboard.py
@@ -0,0 +1,7 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+photos_keyboard = InlineKeyboardMarkup(row_width=1,
+                                       inline_keyboard=[[InlineKeyboardButton(text="Да", callback_data='photos_yes')],
+                                                        [InlineKeyboardButton(text="Нет", callback_data='photos_no')
+                                                         ]
+                                                        ])
