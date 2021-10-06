971c695b4da83dc8c0a5df712f64b9a8411a1381
@kozminskii_fedor <fvk.1972@gmail.com>
2021-10-06 19:16:55 +0300

    Диплом- небольшие поправки

--- log_and_errors/info_logging.py
+++ log_and_errors/info_logging.py
@@ -3,4 +3,4 @@ import logging
 logging.basicConfig(filename='log_file.log', filemode='a',
                     format='%(filename)s [LINE:%(lineno)d] #%(levelname)s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
-logging.warning('This will get logged to a file')
+logging.warning('Это будет записано в log-файл')
